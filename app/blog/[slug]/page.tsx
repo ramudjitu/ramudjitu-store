8f1167f618374b146c320c2fa288b12a4924ebe0
ramudjitu <kanghidayat18@gmail.com>
2026-06-24 18:34:16 +0700

    fix import path BottomNav slug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import BottomNav from "../components/BottomNav";
+import BottomNav from "../../components/BottomNav";
 import Link from "next/link";
 import type { Metadata } from "next";
 import { PortableText, PortableTextComponents } from "@portabletext/react";
