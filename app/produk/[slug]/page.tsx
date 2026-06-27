f764644d6b27bca034fe851551a844762b628e0d
ramudjitu <kanghidayat18@gmail.com>
2026-06-27 10:14:40 +0700

    fix: page.tsx produk

--- app/produk/[slug]/page.tsx
+++ app/produk/[slug]/page.tsx
@@ -1,10 +1,6 @@
-import { getProdukBySlug } from "@/sanity/queries";
 import ProdukClient from "./ProdukClient";
-
-export const revalidate = 60;
 
 export default async function ProdukDetailPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  const produk = await getProdukBySlug(slug);
-  return <ProdukClient slug={slug} produk={produk} />;
+  return <ProdukClient slug={slug} />;
 }
